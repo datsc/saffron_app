ef68a0ce9c9895204cbceab99a010729e43c0f70
ugur <ural.ugur@gmail.com>
2021-11-02 12:26:24 +0100

    reversed

--- application.py
+++ application.py
@@ -4,6 +4,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "hello world changed in branch Ugur!"
+    return "hello world changed in branch Collab!"
+
 if __name__=="__main__":
     app.run("0.0.0.0")
