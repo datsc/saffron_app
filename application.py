6b8a59b3de4736cca0981fa14268a690be19acf6
Carmine <carsomma@gmail.com>
2021-11-02 14:15:43 +0100

    hello from carmine

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "hello Pseudo Saffron!"
+    return "hello Pseudo Saffron from Carmine!"
 
 if __name__=="__main__":
     app.run("0.0.0.0")
