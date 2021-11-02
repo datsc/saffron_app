9fa913665f55c5a9877b8e58c17191255f4f402a
ugur <ural.ugur@gmail.com>
2021-11-02 14:43:02 +0100

    reverted to Carmines version

--- application.py
+++ application.py
@@ -4,9 +4,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def hello_world():
-
-    return "hello Pseudo Saffron from Ugur, Carmine!"
-
+    return "hello Pseudo Saffron from Carmine!"
 
 if __name__=="__main__":
     app.run("0.0.0.0")
