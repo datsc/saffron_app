6eb96a27b608061265cd8a5a1c9af1d9fed93ef4
ugur <ural.ugur@gmail.com>
2021-11-02 12:12:39 +0100

    uu added

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "hello world changed in branch!"
+    return "hello world changed in branch Ugur!"
 
 if __name__=="__main__":
     app.run("0.0.0.0")
